fff1a71dc976f47f9e3f086de1c5d94e733c0227
Tina Gahan <gahantina@gmail.com>
2026-03-07 17:55:02 +0530

    Simple Calculator

---
+++ BASICS/simple_calculator.py
@@ -0,0 +1,7 @@
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+
+print("Addition: ", a + b)
+print("Subtraction : ", a - b)
+print("Multiplication : ", a * b)
+print("Division : ", a // b)
