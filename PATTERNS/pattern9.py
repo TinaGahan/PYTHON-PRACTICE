d1387edc4de3c6314f515c12458a296b2ffd17bd
Tina Gahan <gahantina@gmail.com>
2026-03-21 21:36:28 +0530

    EQUILATERAL TRIANGLE

--- PATTERNS/pattern9.py
+++ PATTERNS/pattern9.py
@@ -1,4 +1,7 @@
-for i in range(5,0, -1 ):
-    for j in range(1,i+1):
-        print(j, end ="")
+n = 5
+for i in range(1, n+1):
+    for j in range(n-i):
+        print(" ", end ="")
+    for j in range(2*i-1):
+        print("*", end="")
     print()
